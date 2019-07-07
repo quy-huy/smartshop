dd03309ea1b2ab7f1a7837debed0cb27d7f3660c
Quy Huy <huy.bq@neo-lab.vn>
2019-07-07 15:52:18 +0700

    heroku

---
+++ server.js
@@ -0,0 +1,18 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+ 
+const app = express();
+ 
+// Serve only the static files form the dist directory
+// Replace the '/dist/<to_your_project_name>'
+app.use(express.static( './dist/shopping-web'));
+ 
+app.get('/*', (req,res)=> {
+  // Replace the '/dist/<to_your_project_name>/index.html'
+  res.sendFile(path.join(__dirname , '/dist/shopping-web/index.html'));
+});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080, ()=>{
+    console.log('Server Started');
+});

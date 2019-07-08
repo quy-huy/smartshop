f7f763b97e0f151238dc4fa4ba9058d77e5b113f
Quy Huy <huy.bq@neo-lab.vn>
2019-07-08 10:23:58 +0700

    create login/register form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
-import {CardComponent} from './card/card.component'
+import {CardComponent} from './card/card.component';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component'
 const routes: Routes = [
   {path: '', component: CardComponent},
-  {path: 'products', component:HomeComponent}
+  {path: 'products', component:HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent}
 ];
 
 @NgModule({
